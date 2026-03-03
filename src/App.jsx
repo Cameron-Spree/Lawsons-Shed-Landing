c93a6bdd1cefcafcea64c3b05781bd7c707792be
Cameron-Spree <cameron.spree@lawsons.co.uk>
2026-03-03 10:48:57 +0000

    Add/Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Variation1 from './pages/Variation1';
 import Variation2 from './pages/Variation2';
 import Variation3 from './pages/Variation3';
 import Footer from './components/Footer';
+import ThemeCustomizer from './components/ThemeCustomizer';
 
 function App() {
     return (
         <>
+            <ThemeCustomizer />
             <main>
                 {/* Continuous scroll experience */}
                 <Variation1 />
