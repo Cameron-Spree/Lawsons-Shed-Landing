1e1d3a9eff0076908855d6f80132c374b720f0d5
Cameron-Spree <cameron.spree@lawsons.co.uk>
2026-03-03 10:26:50 +0000

    Add/Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import Variation1 from './pages/Variation1';
 import Variation2 from './pages/Variation2';
 import Variation3 from './pages/Variation3';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -11,6 +11,7 @@ function App() {
                 <Variation1 />
                 <Variation2 />
                 <Variation3 />
+                <Footer />
             </main>
         </>
     );
