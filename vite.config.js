54de73693b6c827826a50ca51be1a8692f20ea4e
Cameron-Spree <cameron.spree@lawsons.co.uk>
2026-03-03 09:59:53 +0000

    Add/Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import crypto from 'crypto';
-
-// Polyfill for older Node versions in Codespaces
-if (!globalThis.crypto) {
-  globalThis.crypto = {
-    getRandomValues: function (buffer) {
-      return crypto.randomFillSync(buffer);
-    }
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
