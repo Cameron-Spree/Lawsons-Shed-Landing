8d6fe7c3891b986f09d25b148c3c6abb9dbfc83c
Cameron-Spree <cameron.spree@lawsons.co.uk>
2026-03-03 09:44:56 +0000

    Add src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { useState } from 'react';
-import Navigation from './components/Navigation';
+import React from 'react';
 import Variation1 from './pages/Variation1';
 import Variation2 from './pages/Variation2';
 import Variation3 from './pages/Variation3';
 
 function App() {
-    const [activeTab, setActiveTab] = useState('variation1');
-
     return (
         <>
-            <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
-
             <main>
-                {activeTab === 'variation1' && <Variation1 />}
-                {activeTab === 'variation2' && <Variation2 />}
-                {activeTab === 'variation3' && <Variation3 />}
+                {/* Continuous scroll experience */}
+                <Variation1 />
+                <Variation2 />
+                <Variation3 />
             </main>
         </>
     );
